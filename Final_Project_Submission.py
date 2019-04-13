c435bc63dc111a5444c8ce0b0e8c90986872b4b6
Dimas Torres Llorca <d.torresllorca@gmail.com>
2019-04-13 16:07:16 -0400

    Added trip and hour columns to database

--- Final_Project_Submission.py
+++ Final_Project_Submission.py
@@ -96,6 +96,8 @@ def load_data(city, month, day):
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
     # extract month and day of week from Start Time to create new columns
+    # adding hour and trip columns for use in statistics
+    # trip column is the concatination of each start and end station
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
     df['hour'] = df['Start Time'].dt.hour
