c457a0b5e3b4289af18f337e0a84023b6937e3c6
Dimas Torres Llorca <d.torresllorca@gmail.com>
2019-04-13 16:32:28 -0400

    Included 'cities' variable for readability

--- Final_Project_Submission.py
+++ Final_Project_Submission.py
@@ -17,6 +17,8 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
+    cities = ['chicago','new york','washington']
+
     class Illegal(Exception):
         pass
 
@@ -26,7 +28,7 @@ def get_filters():
     while True:
         try:
             city = input('Would you like to see data for Chicago, New York, or Washington?: \n').lower().strip()
-            if city not in ['chicago','new york','washington']:
+            if city not in cities:
                 raise Illegal
             break
         except Illegal:
